a89eb63acf75ec5d4e8e2fee5a5d986eb5abf6e7
Krrish <krrishmahar5@gmail.com>
2024-06-29 20:39:13 +0530

    database and user login feature add! revision still remaining

--- Week4_Python_Prep/microblog/app/__init__.py
+++ Week4_Python_Prep/microblog/app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask_login import LoginManager
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 # going to use other modules by creating instance of flask(module), module is where mainly flaskk.conf will contains
 
-from app import routes
+from app import routes, models
 # importing route at bottom generally avoid circular imports problem i.e, app() -> func() -> app() again
 # note: In Flask, handlers for the application routes are written as Python functions, called view functions.
 # to directly use 'flask run' cmds, set env path of microblog.py with its absolute path
---
+++ Week4_Python_Prep/microblog/app/models.py
@@ -0,0 +1,45 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timezone
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from flask_login import UserMixin
+from app import db, login
+
+class User(UserMixin, db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    # mapping rows with datatype like idL so.Mapped[int] will make it non-nullable and int type while Optional[] mkaesit nullable
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # this method will be called when you print the object of User class, basically reresentation
+    # author and posts are high level integration of two column by relationship func()
+    #  These two attributes allow the application to access the connected user and post entries.
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc)) 
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),index=True)
+    author: so.Mapped[User]= so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return f'<Post {self.body}>'
+# This modules creates the schema of database and for changes like update/delete, we gotta make them -> store then in migration script -> exceute them in sequence & to do that, we need to create a migration script, which is done by alembic
+# in root/ -> open bash -> cmd "flask db init"
